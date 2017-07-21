e9d35f4b2e7f2b216d7e0bf5482ca49df9e510aa
mahongying <mahongying@jkwx.cn>
2017-07-21 13:57:16 +0800

    httpClient 页面静态化技术，网络爬虫

--- src/main/java/com/wjb/controller/WjbController.java
+++ src/main/java/com/wjb/controller/WjbController.java
@@ -1,5 +1,6 @@
 package com.wjb.controller;
 
+import com.alibaba.druid.util.StringUtils;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.wjb.base.BaseController;
@@ -8,8 +9,10 @@ import com.wjb.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Created by Administrator on 2017/7/5.
@@ -40,7 +43,27 @@ public class WjbController extends BaseController{
 
     @ResponseBody
     @GetMapping("hello")
-    public String hello(Integer pageNum,Integer size){
+    public String hello(Integer pageNum, Integer size, HttpServletRequest request){
+        System.out.println(request);
+        String ip = request.getHeader("X-Real-IP");
+        if (!StringUtils.isEmpty(ip) && !"unknown".equalsIgnoreCase(ip)) {
+            return ip;
+        }
+        ip = request.getHeader("X-Forwarded-For");
+        // 多次反向代理后会有多个IP值，第一个为真实IP。
+        if (!StringUtils.isEmpty(ip) && !"unknown".equalsIgnoreCase(ip)){
+            int index = ip.indexOf(",");
+            if (index != -1){
+                ip.substring(0,index);
+            }else {
+                return ip;
+            }
+        }else{
+            return request.getRemoteAddr();
+        }
+        System.out.println(ip);
+
+
         PageHelper.startPage(pageNum == null ? 1 : pageNum,size == null ? 3 : size);
         PageInfo<User> list = userService.all();
         return SUCCESS_FAIL(list.getSize() > 0,list,"error");
--- src/main/java/com/wjb/demo/BigDecimalTest.java
+++ src/main/java/com/wjb/demo/BigDecimalTest.java
@@ -1,17 +1,16 @@
 package com.wjb.demo;
 
-import java.math.BigDecimal;
+import com.wjb.controller.WjbController;
 
 /**
  * Created by Administrator on 2017/7/16.
  */
-public class BigDecimalTest {
-    public static void main(String[] args) {
-        String s = "100.23";
-        double a = 100.23;
-        BigDecimal decimal = new BigDecimal(a);
-        System.out.println(decimal);
-
+public class BigDecimalTest{
+    public static void main(String[] args) throws NoSuchFieldException {
+        Class<WjbController> aClass = WjbController.class;
 
     }
+
+
+
 }
